4205af3ac9895e87e5f8485e2f164b29e7ae5c12
elene638 <elenejavaxishvili@yahoo.com>
2022-05-29 19:39:43 -0700

    css changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+function Navbar () {
+    return (
+        <div className="nav-bar" >
+            <img alt="logo" src="logo192.png" />
+            <h2>ReactFacts</h2>
+            <h3>React Course - Project 1</h3>
+        </div>
+    )
+}
+
+export default Navbar
